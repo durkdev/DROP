f2b71970c7478e69bc7721491a5b5fae4272dd83
J3B4 <j3b@3b1.org>
2020-09-10 01:16:59 +0000

    added dungeon script page to world folder

---
+++ world/dungeon.py
@@ -0,0 +1,9 @@
+# dungeon.py
+"""
+This could be the entire dungeon delving menu system. It can be placed in
+a room near a town to obtain some of the town and shop benefits. 
+
+To properly meet the spec, the menu should actual contain its own town but 
+I'm going to delay that to the end.
+"""
+
