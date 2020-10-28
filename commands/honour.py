2d6cf1befe4abf5521a416608c1c32d920514c4a
J3B4 <j3b@3b1.org>
2020-10-28 02:37:30 +0000

    started honour menu, accept verbs, list items

--- commands/honour.py
+++ commands/honour.py
@@ -10,6 +10,9 @@ their journal entries.
 from evennia import Command
 from evennia.utils import evmenu
 
+ALIASES = ["attack", "kill", "break", "smash", "solve", "pick",
+           "disarm"]
+
 
 class Engage(Command):  # intereact with target object.
     """
@@ -18,15 +21,11 @@ class Engage(Command):  # intereact with target object.
     Usage:
       engage <target>
 
-    Aliases:  attack, kill, break, smash, get, solve, pick, disarm
-
-    Using the most appropriate alias will make your journal more readable
-    but they have no other effect.
+    Aliases:  attack, kill, break, smash, solve, pick, disarm
     """
 
     key = "engage"
-    aliases = ["attack", "kill", "break", "smash", "get", "solve", "pick",
-               "disarm"]
+    aliases = ALIASES
     # Note that aliases will seem to be completely different commands but
     # under the hood they are almost all the same thing. In the future
     # perhaps a parser will do a sanity check. Or maybe not.
@@ -35,17 +34,85 @@ class Engage(Command):  # intereact with target object.
 
     def parse(self):
         # check is there a target?
-        self.caller.msg(self.args)
-        # and does the target exist?
+        parse = "Look in room for target object"
+        self.caller.msg(parse)
 
     def func(self):
         "open the honour menu"
-        self.caller.msg("You plan to %s the %s" % (self.cmdstring, self.args))
+        self.caller.msg("You {} the{}".format(self.cmdstring, self.args))
         evmenu.EvMenu(self.caller,
                       "commands.honour",  # menu nodes in this file
-                      startnode="mn_start")
+                      startnode=("mn_start"),
+                      startnode_input=(self.cmdstring, {"Target": self.args})
+                      )
     # Thats the command. The rest will all play out in the menu. Good luck.
 
 
 def mn_start(caller, raw_string, **kwargs):
     "start up the menu"
+    caller.ndb._menutree.target = kwargs["Target"].strip()
+    text = "How do you plan on {}ing the {}?".format(
+            raw_string, caller.ndb._menutree.target)
+    options = []
+    for verb in ALIASES:
+        # add a option for each of the base ALIASES
+        options.append({"desc": verb,
+                        "goto": ("mn_items", {"verb": verb})})
+    options.append(
+            {"key": ("Other", "o", "O"),
+             "desc": "Write in another verb of your choosing.",
+             "goto": "mn_writeverb"})
+    return text, options
+
+
+def _set_verb(caller, raw_string, **kwargs):
+    "write in an original verb"
+    inp = raw_string.strip()
+    prev_entry = kwargs.get("prev_entry")
+
+    if not inp:
+        if prev_entry:
+            caller.key = prev_entry
+            caller.msg("Set verb to {}.".format(prev_entry))
+            return "mn_items"
+        else:
+            caller.msg("Aborted.")
+            return "node_exit"
+    else:
+        return None, {"prev_entry": inp}
+
+
+def mn_writeverb(caller, raw_string, **kwargs):
+    "check if we have a verb already"
+    prev_entry = kwargs.get("prev_entry")
+    Target = caller.ndb._menutree.target
+
+    if prev_entry:
+        text = "You {} the {}".format(
+                prev_entry, Target)
+        text += "\nEnter a new verb or <return> to accept"
+    else:
+        text = "Enter an original verb or <return> to abort."
+
+    options = {"key": "_default",
+               "goto": (_set_verb, {"prev_entry": prev_entry})}
+    return text, options
+
+
+def mn_items(caller, raw_string, **kwargs):
+    "confirm main verb, choose items"
+    Target = caller.ndb._menutree.target
+    text = "You will {} the {}.\n".format(kwargs["verb"], Target)
+    text += "You have the following items in your inventory. Choose one"
+    text += " it will be instrumental in your attempt. You will have the"
+    text += " chance to choose more than one item."
+
+    options = []
+    for item in caller.contents:
+        options.append({"desc": item,
+                       "goto": "mn_itemcheck"})
+    options.append(
+            {"key": ("Other", "o", "O"),
+             "desc": "Write in another item of your choosing.",
+             "goto": "mn_writeitem"})
+    return text, options
